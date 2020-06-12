7db89246b62049bbf3ec036166fcbf7382d9b951
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-06-12 13:05:29 +0200

    create bar plot for all columns

--- tests/basic/run_dorothea.r
+++ tests/basic/run_dorothea.r
@@ -3,17 +3,19 @@ library(dplyr)
 library(tibble)
 library(ggplot2)
 
+# read arguments
 args = commandArgs(trailingOnly = TRUE)
 dorothea_file = args[1]
 confidence_level = unlist(strsplit(args[2], ","))
 minsize = as.numeric(args[3])
 efilter = as.logical(args[4])
-top_n = as.numeric(args[5])
+topN = as.numeric(args[5])
 
 # output files names
 file_csv = paste0("dorothea_scores_",  paste0(confidence_level, collapse = ""), ".csv")
 file_png = paste0("top_", as.character(top_n), ".png")
 
+# read file to calculate dorothea
 dorothea_matrix <- as.matrix(read.csv(dorothea_file, row.names = 1))
 
 # Calculate dorothea
@@ -27,27 +29,37 @@ tf_activities_stat <- dorothea::run_viper(dorothea_matrix, regulons,
 
 write.csv(tf_activities_stat, file_csv, quote=F)
 
-# figures
+## FIGURES ##
+conditions = colnames(tf_activities_stat)
 
-tf_activities_stat_top25 <- tf_activities_stat %>%
+# Top N TFs based on the normalized enrichment scores in a bar plot per sample
+
+tf_activities_stat <- tf_activities_stat %>%
   as.data.frame() %>%
-  rownames_to_column(var = "GeneID") %>%
-  dplyr::rename(NES = "t") %>%
-  dplyr::top_n(top_n, wt = abs(NES)) %>%
-  dplyr::arrange(NES) %>%
-  dplyr::mutate(GeneID = factor(GeneID))
+  rownames_to_column(var = "GeneID")
 
-topN <- ggplot(tf_activities_stat_top25,aes(x = reorder(GeneID, NES), y = NES)) +
-  geom_bar(aes(fill = NES), stat = "identity") +
-  scale_fill_gradient2(low = "darkblue", high = "indianred",
-                       mid = "whitesmoke", midpoint = 0) +
-  theme_minimal() +
-  theme(axis.title = element_text(face = "bold", size = 12),
-        axis.text.x =
-          element_text(angle = 45, hjust = 1, size =10, face= "bold"),
-        axis.text.y = element_text(size =10, face= "bold"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank()) +
-  xlab("Transcription Factors")
+for(i in condition){
 
-ggsave(filename = file_png, plot = topN)
+  aux <- tf_activities_stat[, c("GeneID", i)] %>%
+    dplyr::rename(NES = "t") %>%
+    dplyr::top_n(topN, wt = abs(NES)) %>%
+    dplyr::arrange(NES) %>%
+    dplyr::mutate(GeneID = factor(GeneID))
+
+  topN_barplot <- ggplot(aux, aes(x = reorder(GeneID, NES), y = NES)) +
+    geom_bar(aes(fill = NES), stat = "identity") +
+    scale_fill_gradient2(low = "darkblue", high = "indianred",
+                         mid = "whitesmoke", midpoint = 0) +
+    theme_minimal() +
+    theme(axis.title = element_text(face = "bold", size = 12),
+          axis.text.x =
+            element_text(angle = 45, hjust = 1, size =10, face= "bold"),
+          axis.text.y = element_text(size =10, face= "bold"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank()) +
+    xlab("Transcription Factors") +
+    ylab("Normalized Enrichment scores (NES)")
+
+  ggsave(filename = file_png, plot = topN_barplot)
+
+}
